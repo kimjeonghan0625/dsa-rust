77986ab60eaad4a5236422579c1db8feae969b60
KIM JEONG HAN <mineship@pusan.ac.kr>
2025-08-07 04:16:16 +0900

    Update combination example to include second method using Pascal's triangle

--- crates/recursive/src/bin/combination/main.rs
+++ crates/recursive/src/bin/combination/main.rs
@@ -1,6 +1,8 @@
-use recursive::combination_util::c;
+use recursive::combination_util::{c, c_with_pascal_triangle};
 
 fn main() {
-    let five_c_two = c(5, 2);
-    println!("{five_c_two}");
+    let five_c_two = c(4, 2);
+    println!("first method: {five_c_two}");
+    let five_c_two = c_with_pascal_triangle(4, 2);
+    println!("second method: {five_c_two}");
 }
--- crates/recursive/src/combination_util/mod.rs
+++ crates/recursive/src/combination_util/mod.rs
@@ -7,3 +7,10 @@ pub fn c(n: i32, r: i32) -> i32 {
     let n_minus_r_fac = factorial(n - r);
     (n_fac / r_fac) / n_minus_r_fac
 }
+
+pub fn c_with_pascal_triangle(n: i32, r: i32) -> i32 {
+    if r == 0 || r == n {
+        return 1;
+    }
+    c_with_pascal_triangle(n-1, r-1) + c_with_pascal_triangle(n-1, r)
+}
